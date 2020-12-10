75cbd2825624d5f8b4a2078ac6aaf4176f526726
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-12-10 16:08:33 +0100

    add file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if ac == 0:
         print('0 arguments.')
     elif ac == 1:
-        print('1 arguments.')
+        print('1 argument:')
         print('1: {}'.format(sys.argv[1]))
     else:
         print('{} arguments.'.format(ac))
