3162483aad8db1f7a395f28b28c029cbd78ace71
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-12-10 16:03:18 +0100

    add file 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    ac = len(sys.argv) - 1
+    if ac == 0:
+        print('0 arguments.')
+    elif ac == 1:
+        print('1 arguments.')
+        print('1: {}'.format(sys.argv[ac]))
+    else:
+        print('{} arguments.'.format(ac))
+        for i in range(1, ac + 1):
+            print('{}: {}'.format(i ,sys.argv[i]))
