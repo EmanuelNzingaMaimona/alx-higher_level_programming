9690f29ec48b7554fa033dcb61fd124777a0b5f8
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2020-12-20 22:27:51 +0100

    add file 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        p = [x for x in sorted(a_dictionary.keys())]
-        return (p[len(p) - 1])
-    return None
+        return (max(a_dictionary.keys()))
