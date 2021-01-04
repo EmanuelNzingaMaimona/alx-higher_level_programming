bef4ebc2d4127c50e257d321e2a56f637dc3acbe
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2021-01-04 15:18:14 +0100

    add file 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@ def safe_print_division(a, b):
     r = 0
     try:
         r = a / b
-    except:
+    except Div:
         r = None
     finally:
         print("Inside result:", r)
-    return r
+        return r
