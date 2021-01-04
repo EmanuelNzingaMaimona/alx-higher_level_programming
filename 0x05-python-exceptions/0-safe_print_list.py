9c371b155307d004070904f894e78ae17ed97441
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2021-01-04 14:48:48 +0100

    add file 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     c = 0
-    try:
-        for i in my_list:
-            if (c > x - 1):
-                break
-            print(i, end = "")
-            c +=1
-        print()
-    except:
-        pass
+    for p in range(x):
+        try:
+            print(my_list[p], end="")
+            c += 1
+        except IndexError:
+            break
+    print('')
     return c
