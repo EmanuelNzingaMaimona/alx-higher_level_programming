842c5f31055be7eca23f3db4dc27acf9b5733dcb
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2021-01-22 17:09:03 +0100

    add file 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-"""Defines a class Student."""
+"""Module for Student class"""
 
 
 class Student:
-    """Represent a student."""
-
+    """Student class"""
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
 
@@ -18,16 +16,13 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
 
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        
-        Args:
-            attrs (list): (Optional) The attributes to represent.
-        """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
+    def to_json(self, attrs=None):
+         """Get a dictionary representation of the Student."""
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for i in attrs:
+            if i is in self.__dict__.keys():
+                dic[i] = self.__dict__[i]
+        return dic
