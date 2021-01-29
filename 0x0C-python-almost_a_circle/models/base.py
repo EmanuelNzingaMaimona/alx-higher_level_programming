8305fd4a1dd0d75139a83038d4e0106d880adafa
EmanuelNzingaMaimona <emanuelmaimona@hotmail.com>
2021-01-29 12:14:24 +0100

    add base module

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -35,7 +35,6 @@ class Base:
             return "[]"
         return json.dumps(list_dictionaries)
 
-
     @classmethod
     def save_to_file(cls, list_objs):
         """Write the JSON serialization of a list of objects to a file.
