bd0c64bba3e2288d03f6afb080f3ca688e794306
Emanuel Nzinga Maimona <emanuelmaimona@hotmail.com>
2021-06-12 21:09:12 +0100

    add file 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,40 +1,16 @@
 #!/usr/bin/python3
-# takes in an argument and displays all values in the states table of
-# hbtn_0e_0_usa where name matches the argument
-# But this time, it is safe from SQL injections
-
-
-def getStates(userName, passWord, dbName, stateName):
-    """ Accesses database hbtn_0e_0_usa and grabs states that begin with 'N'.
-    ARGS:
-        userName: the username
-        passWord: the password
-        dbName: the name of the database to access
-        stateName: the name of the state to check
-    """
-
-    import MySQLdb
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=userName,
-        passwd=passWord,
-        db=dbName,
-        charset="utf8"
-    )
-
-    cur = db.cursor()
-    sqlCommand = "SELECT * FROM states WHERE name = %s;"
-    cur.execute(sqlCommand, (stateName,))
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
-    cur.close()
-    db.close()
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Safe from SQL injections.
+# Usage: ./3-my_safe_filter_states.py <mysql username> \
+#                                     <mysql password> \
+#                                     <database name> \
+#                                     <state name searched>
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """ Take in arguments and passes to get states from db """
-    from sys import argv
-
-    getStates(argv[1], argv[2], argv[3], argv[4])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
