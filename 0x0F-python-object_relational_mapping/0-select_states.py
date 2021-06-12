6567ad371f08b6ba3d8a7b9c90189ef4796ebcfd
Emanuel Nzinga Maimona <emanuelmaimona@hotmail.com>
2021-06-12 21:33:21 +0100

    add file 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
-import sys
-import MySQLdb
+# script that lists all states from the database hbtn_0e_0_usa
+
+
+def getStates(userName, passWord, dbName):
+    """ Accesses database hbtn_0e_0_usa and grabs states, then puts in
+    ascending order.
+    ARGS:
+        userName: the username
+        passWord: the password
+        dbName: the name of the database to access
+    """
+
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=userName,
+        passwd=passWord,
+        db=dbName,
+        charset="utf8"
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    db.close()
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    """ Take in arguments and passes to get states from db """
+    from sys import argv
+
+    getStates(argv[1], argv[2], argv[3])
