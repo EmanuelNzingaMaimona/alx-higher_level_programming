ae7a1abcaa890869468406990e1067347615faa3
Emanuel Nzinga Maimona <emanuelmaimona@hotmail.com>
2021-06-12 21:10:25 +0100

    add file 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-# Module that contains code for Task 12
+# Changes the name of the State object with id = 2 to
+# New Mexico in the database hbtn_0e_6_usa.
+# Usage: ./12-model_state_update_id_2.py <mysql username> /
+#                                        <mysql password> /
+#                                        <database name>
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
 if __name__ == "__main__":
-    """ changes the name of a State object from the database hbtn_0e_6_usa """
-
-    from sys import argv
-    from sqlalchemy import (create_engine)
-    from model_state import Base, State
-    from sqlalchemy.orm import sessionmaker
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id).all():
-        if state.id == 2:
-            state.name = "New Mexico"
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
     session.commit()
-    session.close()
